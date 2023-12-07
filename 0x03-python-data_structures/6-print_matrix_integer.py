aeaff6c638791bb633df2009da144c3550318a60
Qonda Mampofu <mampofuq@gmail.com>
2023-12-07 13:12:21 -0800

    prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for colmn in row:
-            print("{:d}".format(colmn), end=" ")
+    if not matrix:
         print()
+    else:
+        for row in matrix:
+            for idx, colmn in enumerate(row):
+                if idx != len(row) - 1:
+                    space=" "
+                else:
+                    space= ""
+                print("{:d}".format(colmn), end=space)
+            print()
