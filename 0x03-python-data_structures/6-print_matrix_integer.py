dbdd1eeba0f96141aed24b7a5a484bbd6d8c1f7b
Qonda Mampofu <mampofuq@gmail.com>
2023-12-07 13:19:58 -0800

    Pycodestyle validation

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
+    if matrix is None:
         print()
     else:
         for row in matrix:
             for idx, colmn in enumerate(row):
                 if idx != len(row) - 1:
-                    space=" "
+                    space =" "
                 else:
-                    space= ""
+                    space = ""
                 print("{:d}".format(colmn), end=space)
             print()
